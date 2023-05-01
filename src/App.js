0c2d3b024b00763459acda4dc211fab786bda473
Дарига <dariga.syzdykova05@gmail.com>
2023-05-02 03:59:11 +0600

    second load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import 'bulma/css/bulma.css';
 import ProfileCard from "./Components/ProfileCard";
 import AlexaImage from './img/alexa.png';
 import CortanaImage from './img/cortana.png';
@@ -7,22 +7,43 @@ import SiriImage from './img/siri.png';
 function App() {
     return (
         <div className="App">
-            <h1>Personal Digital Assistant</h1>
-            <ProfileCard
-                title="Alexa"
-                handle="@alexa99"
-                image={AlexaImage}
-            />
-            <ProfileCard
-                title="Cortana"
-                handle="@cortana32"
-                image={CortanaImage}
-            />
-            <ProfileCard
-                title="Siri"
-                handle="@siri01"
-                image={SiriImage}
-            />
+            <section className="hero is-primary">
+                <div className="hero-body">
+                    <p className="title">
+                        Personal Digital Assistant
+                    </p>
+                </div>
+            </section>
+            <div className="container">
+                <div className="section">
+                    <div className="columns is-centered">
+                        <div className="column is-2">
+                            <ProfileCard
+                                title="Alexa"
+                                handle="@alexa99"
+                                image={AlexaImage}
+                                description="Alexa was created by Amazon and helps you buy things."
+                            />
+                        </div>
+                        <div className="column is-2">
+                            <ProfileCard
+                                title="Cortana"
+                                handle="@cortana32"
+                                image={CortanaImage}
+                                description="Cortana was created by Microsoft. Who knows what it does?"
+                            />
+                        </div>
+                        <div className="column is-2">
+                            <ProfileCard
+                                title="Siri"
+                                handle="@siri01"
+                                image={SiriImage}
+                                description="Siri was created by Apple. And is being phased out."
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/Components/ProfileCard.js
+++ src/Components/ProfileCard.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
-const ProfileCard = ({title, handle, image}) => {
+const ProfileCard = ({title, handle, image, description}) => {
     //const {title, handle} = props is the same as ({title, handle})
     return (
         <div>
-            <img src={image} alt="pda logo"/>
-            <div>Profile is {title}</div>
-            <div>Handle is {handle}</div>
+            <div className="card">
+                <div className="card-image">
+                    <figure>
+                        <img src={image} alt="pda logo"/>
+                    </figure>
+                </div>
+            </div>
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">{title}</p>
+                    <p className="subtitle is-6">{handle}</p>
+                </div>
+                <div className="content">
+                    {description}
+                </div>
+            </div>
         </div>
     )
 }
